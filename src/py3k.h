92c8f884b203506683008dbdfb1e737352902da2
Joshua Arnott <josh@snorfalorpagus.net>
2015-02-14 22:27:46 +0000

    py3k: use PyUnicode instead of PyBytes

--- src/py3k.h
+++ src/py3k.h
@@ -2,12 +2,12 @@
 
 #if PY_MAJOR_VERSION >= 3
 
-#define PyString_Check PyBytes_Check
-#define PyString_AsString(val) PyBytes_AsString(val)
-#define PyString_FromString(val) PyBytes_FromString(val)
-#define PyString_FromFormat PyBytes_FromFormat
-#define PyString_FromFormat(format, ...) PyBytes_FromFormat(format, __VA_ARGS__)
-#define PyString_Size(val) PyBytes_Size(val)
+#define PyString_Check PyUnicode_Check
+#define PyString_AsString(val) PyUnicode_AsUTF8(val)
+#define PyString_FromString(val) PyUnicode_FromString(val)
+#define PyString_FromFormat PyUnicode_FromFormat
+#define PyString_FromFormat(format, ...) PyUnicode_FromFormat(format, __VA_ARGS__)
+#define PyString_Size(val) PyUnicode_GET_SIZE(val)
 #define PyInt_Type PyLong_Type
 #define PyInt_AS_LONG(val) val
 #define PyInt_Check(val) PyLong_Check(val)
