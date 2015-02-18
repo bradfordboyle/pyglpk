0f4d3e052fac6b259a8ab47cdbb996c3450c3be0
Joshua Arnott <josh@snorfalorpagus.net>
2015-02-18 23:44:11 +0000

    PyString to PyUnicode

--- src/2to3.h
+++ src/2to3.h
@@ -9,6 +9,12 @@
 #define PyInt_Check(o) PyLong_Check(o)
 #define PyInt_FromSize_t(v) PyLong_FromSize_t(v)
 
+#define PyString_FromString(v) PyUnicode_FromString(v)
+#define PyString_AsString(o) PyUnicode_AsUTF8(o)
+#define PyString_Size(o) PyUnicode_GET_SIZE(o)
+#define PyString_FromFormat(fmt, ...) PyUnicode_FromFormat(fmt, __VA_ARGS__)
+#define PyString_Check(o) PyUnicode_Check(o)
+
 #endif
 
 #ifndef PyVarObject_HEAD_INIT
