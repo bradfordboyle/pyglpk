34a0cae91d2dbd15eb785547a709d5469191c4d5
Joshua Arnott <josh@snorfalorpagus.net>
2015-02-19 00:14:03 +0000

    Fixed typo in PyInt_AsLong to PyLong_AsLong conversion.

--- src/2to3.h
+++ src/2to3.h
@@ -4,7 +4,7 @@
 
 #define PyInt_FromLong(v) PyLong_FromLong(v)
 #define PyInt_AS_LONG(v) PyLong_AsLong(v)
-#define PyInt_AsLong(v) PyLong_AsLong
+#define PyInt_AsLong(v) PyLong_AsLong(v)
 #define PyInt_Type PyLong_Type
 #define PyInt_Check(o) PyLong_Check(o)
 #define PyInt_FromSize_t(v) PyLong_FromSize_t(v)
